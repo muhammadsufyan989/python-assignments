55dea36959301307f65b8dc44ac09bc2b36867e1
Muhammad Sufyan <sufyanashraf989@gmail.com>
2019-11-08 23:49:30 +0500

    ASSIGNMENT SUBMITTED

---
+++ Python  assignment 1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Twinkle, twinkle, little star,")
+print("       How I wonder what you are,")
+print("            Up above the world so high,")
+print("            Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("       How I wonder what you are,")
+
+
+# In[2]:
+
+
+import sys
+print("Python version")
+print (sys.version)
+
+
+# In[9]:
+
+
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+
+# In[5]:
+
+
+r = int(input("Enter the radius "))
+area = 3.142*(r**2)
+print("Area of the Circle : ")
+print(area)
+
+
+# In[6]:
+
+
+first_name = input("Please enter your first name : ")
+last_name = input("Please enter your last name : ")
+full_name = last_name +" "+ first_name
+print(full_name)
+
+
+# In[8]:
+
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = a + b
+print ("The sum is :")
+print (c)
+
+
+# In[ ]:
+
+
+
+
